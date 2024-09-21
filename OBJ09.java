6fd1032238a564f8f0e1ed1a8c293102133ef5a0
chewmonguy225 <mikeylupa225@gmail.com>
2024-09-21 11:12:19 -0500

    Update OBJ09.java - added Javadoc comments

--- OBJ09.java
+++ OBJ09.java
@@ -1,9 +1,14 @@
 public class OBJ09 {
+    /**
+     * Main method demonstrates how to compare the classes of two objects 
+     * using the getClass() method. (OBJ09-J compliant)
+     */
     public static void main(String[] args) {
+        // Creates two Object instances of type String
         Object obj1 = new String("Hello");
         Object obj2 = new String("World");
 
-        //Compare classes directly, not class names
+        // Compare classes directly using getClass() rather than class names.
         if (obj1.getClass() == obj2.getClass()) {
             System.out.println("same class.");
         } else {
@@ -11,3 +16,4 @@ public class OBJ09 {
         }
     }
 }
+
