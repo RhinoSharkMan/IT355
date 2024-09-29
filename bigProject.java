f0b1b77135caa21fce3c4d37b830963a76acc032
Chase Mayberry <156947914+chasejmReal@users.noreply.github.com>
2024-09-29 18:46:22 -0500

    Update

--- bigProject.java
+++ bigProject.java
@@ -1,8 +1,9 @@
+
 /**
  * IT 355 - Group Project 1
- * Demonstration of rule ERR04-J with proper usage 
+ * 
  */
-class ERR04_J
+class bigProject
 {
 
 /**
@@ -14,15 +15,6 @@ class ERR04_J
     }
     
 
-/**
- * Example of ERR04-J - not abrubtly returning within the finally block
- *
- * This method returns 0 if no exception occurs. If an exception occurs,
- * it catches it and returns 1. Regardless of what happens, it will execute
- * the code in the finally block to perform any necessary cleanup.
- * 
- * @return 1 if no exception occurs, 2 if an exception occurs
- */
     public static int exampleMethod() {
         try {
             System.out.println("hello world");
