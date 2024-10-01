e9047a086183888226f06078920c468ea250b1d3
Tweagle32 <tweagle@ilstu.edu>
2024-10-01 10:58:16 -0500

    Add files via upload

---
+++ FIO08.java
@@ -0,0 +1,14 @@
+import java.io.FileInputStream;
+
+public class FIO08 {
+    FileInputStream in;
+    // Initialize Stream
+    int intBuffer;
+    char infromationData;
+    while ((intBuffer = in.read()) != -1) {
+        infromationData = (char) intBuffer;
+
+    }
+
+    
+}}
