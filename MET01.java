db9d22e9369378e9db569669a745987ded9eaa3f
apreppen <113079105+apreppen@users.noreply.github.com>
2024-10-01 09:42:31 -0500

    Update MET01.java
    Fixing javadoc

--- MET01.java
+++ MET01.java
@@ -12,10 +12,12 @@ public class MET01 {
     joinStrings(word1,word2); 
     }
 
-    //joins two strings, uses exception handling that does not use the assertion keyword.
-    //@param s1 first string to be joined
-    //@param s2 second string to be joined
-    //@return joined strings if valid, if one or more strings is null returns print statement
+ /** 
+    * joins two strings, uses exception handling that does not use the assertion keyword.
+    * @param s1 first string to be joined
+    * @param s2 second string to be joined
+    * @return joined strings if valid, if one or more strings is null returns print statement
+    */
     public static void joinStrings(String s1, String s2)
     {
         if(s1 == null|| s2 == null)
