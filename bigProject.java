c669caf48afceb2a55399e1ab81ef7e9287ef1c7
apreppen <113079105+apreppen@users.noreply.github.com>
2024-10-03 12:08:05 -0500

    Update bigProject.java

--- bigProject.java
+++ bigProject.java
@@ -5,6 +5,7 @@
 
 
 import java.util.Scanner;
+import java.util.Random; 
 
 public class bigProject {
 
@@ -42,6 +43,32 @@ public class bigProject {
                     // Add logic for option 100
                     System.out.println("You selected OPTION 100.");
                     break;
+                case 81: 
+                    System.out.println("You selected OPTION 81.");
+                    System.out.println("Please enter patient's room number to validate"); 
+                    int roomNum;
+
+                    String input = scanner.nextLine();
+                    if (isInteger(input))
+                    {
+                        roomNum = input; 
+                    }
+                    else 
+                    {
+                      System.out.println("Invalid input"); 
+                      break; 
+                    }
+
+                    if (checkRoomNumber(roomNum))
+                    {
+                        System.out.println("Valid room number entered"); 
+                    }
+                    else
+                    {
+                        System.out.println("Room number incorrect, please contact patient's medical staff to find correct room"); 
+                    }
+                    break; 
+                    
                 default:
                     System.out.println("Invalid option. Please try again.");
             }
@@ -71,5 +98,118 @@ public class bigProject {
     }
 
 
+/*
+* Utilizing MET00-J by validating the method's arguments. 
+* Also satisfies MET01-J by not using assertions to validate the argument. 
+* Hospital has 50 floors, room number has to 101 - 5099
+* @param room integer describing hospital's room number
+* @return true if room is in correct range, false otherwise. 
+*/
+public static bool checkRoomNumber(int room)
+    {
+        //validating arguments
+        if (num >= 101 && num <= 5099)
+        {
+           return true; 
+        }
+
+        else
+        {
+          return false; 
+        }
+    }
     
 } //end class
+
+
+
+class HospitalEmployee
+{
+    private int HID; 
+    private String name; 
+    private double payRate; 
+    private String profession; 
+    private int securityLevel; 
+    protected int floorLevel; 
+    
+    HospitalEmployee(int HID, String name, double payRate, String profession)
+    {
+        this.HID = HID;
+        this.name = name; 
+        this.payRate = payRate; 
+        this.profession = profession; 
+
+        securityLevel = initialSecurityLevel(profession)
+
+        floorLevel = designateFloor(); 
+     }
+
+    /*
+    * Satisfied MET05-J, constructor calls this method which is final so it is not overridable. 
+    */
+    private final int initialSecurityLevel(String jobTitle)
+    {
+        if (jobTitle == "CEO" || jobTitle == "Executive")
+        {
+            return 5;
+        }
+
+        else if (jobTitle == "Security")
+        {
+            return 4; 
+        }
+        
+        else if (jobTitle == "Doctor" || jobTitle == "Surgeon")
+        {
+            return 3; 
+        }    
+
+        else if (jobTitle == "Nurse")
+        {
+            return 2; 
+        }
+
+        else if (jobTitle == "Janitor" || jobTitle == "Maintenance" || jobTitle == "Electrician")
+        {
+            return 1;
+        }
+
+        else
+        {
+            return 0; 
+        }
+    }
+
+    protected int designateFloor()
+    {
+        Random randNum = new Random(); 
+        int floorNum = randNum.nextInt(50) + 1; 
+    }
+}
+
+class HospitalJanitor extends HospitalEmployee
+{
+    int lowestFloorNum;
+    int highestFloorNum; 
+
+    HospitalJanitor(int HID, String name, double payRate, String profession)
+    {
+        super(int HID, String name, double payRate, String profession); 
+        
+        lowestFloorNum = designateFloor() 
+    }
+    
+    @Override
+    //janitors get multiple floors to work on
+    private int designateFloor()
+    {
+        Random randNum = new Random(); 
+        int floorNum = randNum.nextInt(9) + 1 * 5; 
+        //lowest floor num is multiple of 5 from 1 - 45, highest will be that + 5; 
+    }
+    //override designateFloor method, keep int unaccessible for MET04-J
+
+    
+}
+
+
