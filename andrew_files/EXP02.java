058b0b7cd4b8e748a51a4e9c7c68b0ab814e4c11
Chase Mayberry <156947914+chasejmReal@users.noreply.github.com>
2024-10-13 18:19:51 -0500

    Organzing

---
+++ andrew_files/EXP02.java
@@ -0,0 +1,16 @@
+package andrew_files;
+import java.util.Arrays;
+public class EXP02 {
+    public static void main(String[] args) 
+    {
+        int[] array1 = {1,3,5,6}; 
+        int[] array2 = {1,3,5,6};
+        int[] array3 = {1,3,5,7,9};
+
+        //calling Arrays.equals method when arrays are equal
+        System.out.println(Arrays.equals(array1,array2)); 
+        //calling Arrays.equals method when arrays are not equal
+        System.out.println(Arrays.equals(array1,array3)); 
+    }
+
+}
---
+++ andrew_files/VNA00.java
@@ -0,0 +1,34 @@
+
+public class VNA00 implements Runnable {
+private static volatile int value = 0; 
+//main methods creates two threads, starting them calls run function
+public static void main(String [] args)
+{
+Thread thread1 = new Thread(new VNA00());
+Thread thread2 = new Thread(new VNA00());
+
+thread1.start();
+thread2.start();
+}
+
+//run function for threads, first thread gets interrupted after call, 2nd thread continues
+//by adding 10 to value again and printing
+@Override
+public void run()
+{
+  value = addition(value,10);
+  System.out.println(value);  
+ Thread.currentThread().interrupt();
+}
+
+/**
+* adds two integers together
+* @param val - integer value used to represent value variable
+* @param add - integer value used to represent what is being added to value
+* @return integer value resulting from addition of val and add.   
+*/  
+public int addition(int val, int add)
+{
+   return val + add; 
+}
+}
